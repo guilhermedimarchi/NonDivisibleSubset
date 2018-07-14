c70a8f2bac288ac798ba2d5dbe2e63a819956a9b
Guilherme Dimarchi <guilherme.dimarchi@outlook.com>
2018-07-14 02:03:30 -0300

    Corrigindo um if que estava com a lógica errada e melhorando a documentação

--- src/NonDivisibleSubset/NonDivisibleSubset.java
+++ src/NonDivisibleSubset/NonDivisibleSubset.java
@@ -4,45 +4,62 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+/**
+ * @author Guilherme Dimarchi
+ *
+ */
 public class NonDivisibleSubset {
 
+	/**
+	 * Given a set 's' of 'n' distinct integers, returns the size of a maximal
+	 * subset of 's' where the sum of any 2 numbers is not evenly divisble by k
+	 * @param Integer k
+	 * @param List<Integer> s
+	 * @return int
+	 */
 	public int findNonDivisibleSubset(Integer k, List<Integer> s) {
 		Map<Integer, Integer> mapCounterLeftover = new HashMap<>();
 		int result = 0;
 		
-		 /*Aqui é varrido todas as entradas e calculando o resto da divisão de s[i] por k. Os resultados são acumulados utilizando o resto como chave e a contagem como valor*/
+		/* 
+		 * Creates a map for each leftover possible, counting how many numbers have the same leftover.
+		 */
 		for (int i = 0; i < s.size(); i++) {
 			int leftover = s.get(i) % k;
 			Integer currentCounter = mapCounterLeftover.containsKey(leftover) ? mapCounterLeftover.get(leftover) + 1 : 1;
 			mapCounterLeftover.put(leftover, currentCounter);
 		}
 		
-		/*Se contem algo que gere um resto 0, o valor da chave 0 do mapa será 1 ou mais. Porém podemos considerar apenas um desses números do subset
-		 *Caso contrario a somatória de dois numeros divisiveis por k resulta em outro também divisível. Portanto considerar no máximo 1*/
+		/*
+		 * There may be more than one value that is evenly divisible by k. When this happens we can consider only one of these numbers in our subset.
+		 * Otherwise, adding two numbers that are divisible k results in another divisible number.
+		 */
 		if(mapCounterLeftover.containsKey(0)) {
 			result += 1;
 		}
 		
-		/*Para numeros pares segue a mesma regra do if acima, pois podemos considerar apenas um nnúmero par no subset. 
-		 * Dado que x numeros pares somados sempre serão divisiveis por outro par*/
-		if (k % 2 == 0) {
-			result += 1;
-		}
-		
-		/*Itera apenas metade do k, lendo os valores quesão complementares (val1 e val2). 
-		 * Necessário considerar apenas o val que apresenta uma contagem maior, pois estamos buscando a maior subset.
-		 * Não podemos considerar os dois pois são complementares, e portanto quando considerados juntos geral valores divisiveis por k*/
-		for (int i = 1; i < (k / 2) + 1; i++) {
+		/*
+		 * Iterates half of K, going through the value and its complement. And we count only the one with the bigger counter.
+		 * We can't count both value and complement because adding them would result in a number that is divisible by k.
+		 */
+		for (int i = 1; i <= (k / 2) ; i++) {
 			if (i != k - i) {
 				Integer value = mapCounterLeftover.containsKey(i) ? mapCounterLeftover.get(i) : 0;
 				Integer complement = mapCounterLeftover.containsKey(k - i) ? mapCounterLeftover.get(k - i) : 0;
 				result += Math.max(value, complement);
 			}
 		}
+		
+		/*
+		 * If k is even, the loop above will skip the central index. 
+		 * So for even numbers the central index will have a combination of same number. So we count only one.
+		 */
+		if (k % 2 == 0) {
+			if(mapCounterLeftover.containsKey(k/2)) {
+				if(mapCounterLeftover.get(k/2) > 0)
+					result += 1;
+			}
+		}
 		return result;
 	}
-
-	
-	
-	
 }
